0e7e26093530efd8c8722c86e7ec2cf1d05c61c2
Dima <mityahearthstone@gmail.com>
2025-12-15 20:07:37 +0200

    Add project files.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,22 @@
+﻿var random = new Random();
+int[] nums = new int[10];
+int kos = 0;
+for (int i = 0; i < 10; i++)
+{
+    nums[i] = random.Next(-10, 11);
+}
+for (int i = 0; i < 10; i++)
+{
+    kos = kos + nums[i];
+    Console.Write(nums[i]);
+    Console.WriteLine();
+}
+
+Console.WriteLine("sum of elements");
+Console.WriteLine(kos);
+
+if (kos > 0)
+{
+    Console.WriteLine("Yes");
+}
+else Console.WriteLine("No");
